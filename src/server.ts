d64f6e895d8b3879e11ecf82bd30e944b2139056
andre82br <andre82br@gmail.com>
2026-03-22 20:59:57 -0300

    CRUD

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,13 @@ import { pool } from "./config/database";
 
 dotenv.config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+const dbHost = process.env.DB_HOST || "127.0.0.1";
+console.log("HOST FINAL USADO:", dbHost);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
