bb8e0970b2d330977774db42fde48a0369e48c2f
andre82br <andre82br@gmail.com>
2026-03-22 21:11:51 -0300

    CRUD 20260322-2111

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { pool } from "./config/database";
 
-dotenv.config();
+//dotenv.config();
 
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
